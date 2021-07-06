c41706dd59331478df0909a0a9e716f2760594fa
Josip Prga <josipprga@gmail.com>
2021-07-06 13:31:36 +0200

    Init spring boot app

---
+++ src/main/java/com/example/ratingappbackend/RatingAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ratingappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatingAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ratingappbackend/RatingAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ratingappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
