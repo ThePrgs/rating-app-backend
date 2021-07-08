305990f8c678a244bff1906513b19340fb94796a
ThePrgs <josipprga@gmail.com>
2021-07-08 13:52:01 +0200

    Add user entity and services

---
+++ src/main/java/com/example/ratingappbackend/appuser/AppUser.java
@@ -0,0 +1,78 @@
+package com.example.ratingappbackend.appuser;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Collections;
+
+
+@Getter
+@Setter
+@Entity
+@EqualsAndHashCode
+@NoArgsConstructor
+public class AppUser implements UserDetails {
+    @SequenceGenerator(
+            name = "app_user_sequence",
+            sequenceName = "app_user_sequence",
+            allocationSize = 1
+    )
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "app_user_sequence"
+    )
+
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private AppUserRole appUserRole;
+
+    public AppUser(String firstName, String lastName, String email,String password, AppUserRole appUserRole){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.email=email;
+        this.password=password;
+        this.appUserRole=appUserRole;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+       SimpleGrantedAuthority authority = new SimpleGrantedAuthority(appUserRole.name());
+       return Collections.singletonList(authority);
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/example/ratingappbackend/appuser/AppUserRole.java
@@ -0,0 +1,5 @@
+package com.example.ratingappbackend.appuser;
+
+public enum AppUserRole {
+    USER
+}
