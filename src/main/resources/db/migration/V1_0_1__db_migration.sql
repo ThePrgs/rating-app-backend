36bd3ac3a985f61d3ea374ed114386b10055f64a
ThePrgs <josipprga@gmail.com>
2021-07-09 10:47:56 +0200

    Add sql script for user table

---
+++ src/main/resources/db/migration/V1_0_1__db_migration.sql
@@ -0,0 +1,3 @@
+CREATE SEQUENCE app_user_sequence;
+CREATE TABLE app_user (id bigint, first_name varchar(25), last_name varchar(25), email varchar(50), password varchar(255), app_user_role varchar(15));
+
