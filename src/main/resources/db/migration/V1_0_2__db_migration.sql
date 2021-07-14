abcc97754c8cca86db38db823279ea5819742ff7
ThePrgs <josipprga@gmail.com>
2021-07-14 14:26:13 +0200

    Update sql scripts for emoji order

--- src/main/resources/db/migration/V1_0_2__db_migration.sql
+++ src/main/resources/db/migration/V1_0_2__db_migration.sql
@@ -1,4 +1,4 @@
 CREATE TABLE emoji (id bigint PRIMARY KEY , name varchar(25), color varchar(25), image varchar(50));
-INSERT INTO emoji VALUES (5, 'VERY_HAPPY', 'rgb(0, 168, 107)', 'emoticon-outline.svg'),(4, 'HAPPY', 'rgb(65, 179, 233)', 'emoticon-happy-outline.svg'),
-                         (3, 'MEH', 'rgb(122, 122, 122)', 'emoticon-neutral-outline.svg'), (2, 'SAD', 'rgb(255, 186, 19)', 'emoticon-sad-outline.svg'),
-                         (1, 'VERY_SAD', 'rgb(249, 88, 90)', 'emoticon-angry-outline.svg');
+INSERT INTO emoji VALUES (1, 'VERY_HAPPY', 'rgb(0, 168, 107)', 'emoticon-outline.svg'),(2, 'HAPPY', 'rgb(65, 179, 233)', 'emoticon-happy-outline.svg'),
+                         (3, 'MEH', 'rgb(122, 122, 122)', 'emoticon-neutral-outline.svg'), (4, 'SAD', 'rgb(255, 186, 19)', 'emoticon-sad-outline.svg'),
+                         (5, 'VERY_SAD', 'rgb(249, 88, 90)', 'emoticon-angry-outline.svg');
