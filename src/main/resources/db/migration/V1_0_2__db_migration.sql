af5e7cc32bf0a16cc2c9c592c62b898324d6c6c5
mrakic96 <matej.rakic96@gmail.com>
2021-07-29 15:33:44 +0200

    Update image size in emoji migration

--- src/main/resources/db/migration/V1_0_2__db_migration.sql
+++ src/main/resources/db/migration/V1_0_2__db_migration.sql
@@ -1,4 +1,4 @@
-CREATE TABLE emoji (id bigint PRIMARY KEY , name varchar(25), color varchar(25), image varchar(50));
+CREATE TABLE emoji (id bigint PRIMARY KEY , name varchar(25), color varchar(25), image varchar(255));
 INSERT INTO emoji VALUES (1, 'VERY_HAPPY', 'rgb(0, 168, 107)', 'https://res.cloudinary.com/dxlyytkww/image/upload/v1627541878/RatingApp/very_happy_ujm04f.svg'),
 						 (2, 'HAPPY', 'rgb(65, 179, 233)', 'https://res.cloudinary.com/dxlyytkww/image/upload/v1627541876/RatingApp/happy_mlvvqt.svg'),
 						 (3, 'MEH', 'rgb(122, 122, 122)', 'https://res.cloudinary.com/dxlyytkww/image/upload/v1627541877/RatingApp/meh_owc1iz.svg'),
