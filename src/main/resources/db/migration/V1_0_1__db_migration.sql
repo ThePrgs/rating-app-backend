653d861302a26ef664fed513cef9fcb839fe41f6
mrakic96 <matej.rakic96@gmail.com>
2021-07-29 14:38:17 +0200

    Add db user seeder script

--- src/main/resources/db/migration/V1_0_1__db_migration.sql
+++ src/main/resources/db/migration/V1_0_1__db_migration.sql
@@ -1,4 +1,3 @@
 CREATE SEQUENCE app_user_sequence;
 CREATE TABLE app_user (id bigint, email varchar(50) UNIQUE , app_user_role varchar(15));
-INSERT INTO app_user(id, email, app_user_role) VALUES (1, 'josipprga@gmail.com', 'ADMIN');
-
+INSERT INTO app_user(id, email, app_user_role) VALUES (1, 'josipprga@gmail.com', 'ADMIN'), (2, 'matej.rakic96@gmail.com', 'ADMIN'), (3, 'ivanand34@gmail.com', 'ADMIN'), (4, 'brzicamona@gmail.com', 'ADMIN');
