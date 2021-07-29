3de4cf1c941720a756d6d44eff69a9848d50e484
mrakic96 <matej.rakic96@gmail.com>
2021-07-29 15:15:39 +0200

    Format sql scripts

--- src/main/resources/db/migration/V1_0_2__db_migration.sql
+++ src/main/resources/db/migration/V1_0_2__db_migration.sql
@@ -1,4 +1,6 @@
 CREATE TABLE emoji (id bigint PRIMARY KEY , name varchar(25), color varchar(25), image varchar(50));
-INSERT INTO emoji VALUES (1, 'VERY_HAPPY', 'rgb(0, 168, 107)', 'very_happy.svg'),(2, 'HAPPY', 'rgb(65, 179, 233)', 'happy.svg'),
-                         (3, 'MEH', 'rgb(122, 122, 122)', 'meh.svg'), (4, 'SAD', 'rgb(255, 186, 19)', 'sad.svg'),
-                         (5, 'VERY_SAD', 'rgb(249, 88, 90)', 'very_sad.svg');
+INSERT INTO emoji VALUES (1, 'VERY_HAPPY', 'rgb(0, 168, 107)', 'https://res.cloudinary.com/dxlyytkww/image/upload/v1627541878/RatingApp/very_happy_ujm04f.svg'),
+                         (2, 'HAPPY', 'rgb(65, 179, 233)', 'https://res.cloudinary.com/dxlyytkww/image/upload/v1627541876/RatingApp/happy_mlvvqt.svg'),
+                         (3, 'MEH', 'rgb(122, 122, 122)', 'https://res.cloudinary.com/dxlyytkww/image/upload/v1627541877/RatingApp/meh_owc1iz.svg'),
+                         (4, 'SAD', 'rgb(255, 186, 19)', 'https://res.cloudinary.com/dxlyytkww/image/upload/v1627541878/RatingApp/sad_vokr45.svg'),
+                         (5, 'VERY_SAD', 'rgb(249, 88, 90)', 'https://res.cloudinary.com/dxlyytkww/image/upload/v1627541878/RatingApp/very_sad_rwkai6.svg');
