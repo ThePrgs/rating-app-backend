5550c59390dd0864fd0aab5733c58075cc3b491e
mrakic96 <matej.rakic96@gmail.com>
2021-08-02 15:44:00 +0200

    Update migration scripts

--- src/main/resources/db/migration/V1_0_4__db_migration.sql
+++
@@ -1,3 +0,0 @@
-CREATE SEQUENCE rating_sequence;
-CREATE TABLE rating (id bigint AUTO_INCREMENT, emoji_id bigint ,date DateTime, PRIMARY KEY (id), constraint fk_emoji foreign key(emoji_id) references emoji(id));
-
