ccc082bd50114aa9a350a8373416c02c730c00f9
mrakic96 <matej.rakic96@gmail.com>
2021-08-11 13:58:29 +0200

    Add JavaDoc

--- src/main/java/com/nsoft/ratingappbackend/security/config/PusherConfig.java
+++ src/main/java/com/nsoft/ratingappbackend/security/config/PusherConfig.java
@@ -5,12 +5,20 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Configuration for Pusher - a WebSockets solution.
+ */
 @Configuration
 @AllArgsConstructor
 public class PusherConfig {
 
 	private final AppProperties appProperties;
 
+	/**
+	 * Method configures Pusher and returns a Singleton Pusher Bean.
+	 *
+	 * @return Pusher
+	 */
 	@Bean
 	public Pusher getPusher() {
 		Pusher pusher = new Pusher(appProperties.getPusherAppId(), appProperties.getPusherKey(),
